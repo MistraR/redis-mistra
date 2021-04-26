331391656643a34b3d2d3e46a19325cd247d2750
MistraR <842404548@qq.com>
2021-04-27 00:02:58 +0800

    Initial commit

---
+++ ms-registry/src/main/java/com/mistra/redis/RegistryApplication.java
@@ -0,0 +1,23 @@
+package com.mistra.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2021/4/26 下午11:58
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class RegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
+}
