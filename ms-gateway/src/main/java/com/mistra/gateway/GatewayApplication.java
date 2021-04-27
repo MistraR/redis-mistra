9edb013f26c9ef39f8cee60517fa07ace3dfa6c8
MistraR <842404548@qq.com>
2021-04-27 23:31:48 +0800

    feat:GatewayApplication

---
+++ ms-gateway/src/main/java/com/mistra/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.mistra.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2021/4/27 上午12:05
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
