c68ba15125a9269e22c0e635cb7191ebf285a984
MistraR <842404548@qq.com>
2021-04-28 21:12:01 +0800

    feat:DinersApplication

---
+++ ms-diners/src/main/java/com/mistra/diners/DinersApplication.java
@@ -0,0 +1,21 @@
+package com.mistra.diners;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2021/4/27 上午12:15
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@SpringBootApplication
+public class DinersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DinersApplication.class, args);
+    }
+}
