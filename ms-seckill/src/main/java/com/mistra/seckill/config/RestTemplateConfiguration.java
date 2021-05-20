21fabc7916bffb30eb467636dda93e00435ce2c1
MistraR <842404548@qq.com>
2021-05-20 19:48:20 +0800

    feat:秒杀

---
+++ ms-seckill/src/main/java/com/mistra/seckill/config/RestTemplateConfiguration.java
@@ -0,0 +1,34 @@
+package com.mistra.seckill.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2021/5/7 下午9:01
+ * @ Description:
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+@Configuration
+public class RestTemplateConfiguration {
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.TEXT_PLAIN));
+        restTemplate.getMessageConverters().add(converter);
+        return restTemplate;
+    }
+    
+}
