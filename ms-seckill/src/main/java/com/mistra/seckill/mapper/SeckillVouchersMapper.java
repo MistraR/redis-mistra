382329dd0481dc789cdaf21e37ee9393ca8e32ae
MistraR <842404548@qq.com>
2021-05-23 13:43:15 +0800

    feat:秒杀

---
+++ ms-seckill/src/main/java/com/mistra/seckill/mapper/SeckillVouchersMapper.java
@@ -0,0 +1,33 @@
+package com.mistra.seckill.mapper;
+
+import com.mistra.commons.model.pojo.SeckillVouchers;
+import org.apache.ibatis.annotations.*;
+
+/**
+ * @author Mistra
+ * @ Version: 1.0
+ * @ Time: 2021/5/7 下午9:01
+ * @ Description: 秒杀代金券 Mapper
+ * @ Copyright (c) Mistra,All Rights Reserved.
+ * @ Github: https://github.com/MistraR
+ * @ CSDN: https://blog.csdn.net/axela30w
+ */
+public interface SeckillVouchersMapper {
+
+    // 新增秒杀活动
+    @Insert("insert into t_seckill_vouchers (fk_voucher_id, amount, start_time, end_time, is_valid, create_date, update_date) " +
+            " values (#{fkVoucherId}, #{amount}, #{startTime}, #{endTime}, 1, now(), now())")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    int save(SeckillVouchers seckillVouchers);
+    
+    // 根据代金券 ID 查询该代金券是否参与抢购活动
+    @Select("select id, fk_voucher_id, amount, start_time, end_time, is_valid " +
+            " from t_seckill_vouchers where fk_voucher_id = #{voucherId}")
+    SeckillVouchers selectVoucher(Integer voucherId);
+
+    // 减库存
+    @Update("update t_seckill_vouchers set amount = amount - 1 " +
+            " where id = #{seckillId}")
+    int stockDecrease(@Param("seckillId") int seckillId);
+
+}
